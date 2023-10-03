b813539fef0f45e39b70cd4fb829ba2f90749a2f
Erick Augusto <erickaugs@gmail.com>
2023-10-02 21:25:42 -0300

    primeiro commit

---
+++ src/main/java/Listas/TestandoListas.java
@@ -0,0 +1,35 @@
+package Listas;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class TestandoListas {
+    public static void main(String[] args) {
+
+        String carro1 = "Voyage 1.6";
+        String carro2 = "Palio Weekden 1.8";
+        String carro3 = "AirCross 1.6 - 2011";
+        String carro4 = "Amarok";
+
+        List<String> colecaoCarros = new ArrayList<>();
+        colecaoCarros.add(carro1);
+        colecaoCarros.add(carro2);
+        colecaoCarros.add(carro3);
+        colecaoCarros.add(carro4);
+
+        //System.out.println(colecaoCarros);
+
+//        colecaoCarros.removeAll(colecaoCarros);
+//
+//        System.out.println(colecaoCarros);
+
+//        for (String carro: colecaoCarros) {
+//         System.out.println(carro);
+//        }
+
+        Collections.sort(colecaoCarros);
+        System.out.println(colecaoCarros);
+    }
+}
